0ef25390d658fd73324a9c0d13d7e4fbb4bd780c
DogukanTopallar <dtopallar1@gmail.com>
2023-04-15 09:20:56 +0300

    Comments have been added.

--- ChainOfResponsibility/ChainOfResponsibility/Program.cs
+++ ChainOfResponsibility/ChainOfResponsibility/Program.cs
@@ -3,14 +3,20 @@
 namespace ChainOfResponsibility
 {
     using System;
-    using System;
 
+
+    //IPaymentProcessor: An interface that defines the behavior of payment processors to be used for payment processing.
+    //This interface will be implemented by the classes that form the links in the payment processing chain
     interface IPaymentProcessor
     {
         void ProcessPayment(decimal amount);
         void SetNextProcessor(IPaymentProcessor processor);
     }
 
+    //Represents the payment processor to be used for processing credit card payments. 
+    //This class implements the IPaymentProcessor interface and forms a link in the payment processing chain. 
+    //It can process transactions that are below the amount that can be handled.
+    //If the transaction amount is above the limit, it will forward the transaction to the processor in the next link in the chain.
     class CreditCardPaymentProcessor : IPaymentProcessor
     {
         private IPaymentProcessor _nextProcessor;
@@ -37,7 +43,10 @@ namespace ChainOfResponsibility
             }
         }
     }
-
+    //Represents the payment processor to be used for processing PayPal payments.
+    //This class implements the IPaymentProcessor interface and forms a link in the payment processing chain.
+    //It can process transactions that are below the amount that can be handled.
+    //If the transaction amount is above the limit, it will forward the transaction to the processor in the next link in the chain.
     class PayPalPaymentProcessor : IPaymentProcessor
     {
         private IPaymentProcessor _nextProcessor;
@@ -64,7 +73,9 @@ namespace ChainOfResponsibility
             }
         }
     }
-
+    //Represents the payment processor to be used for processing bank transfer payments.
+    //This class implements the IPaymentProcessor interface and forms the last link in the payment processing chain.
+    //Therefore, it handles the payment processing in cases where all previous links in the chain cannot process the transaction.
     class BankTransferPaymentProcessor : IPaymentProcessor
     {
         private IPaymentProcessor _nextProcessor;
@@ -79,7 +90,10 @@ namespace ChainOfResponsibility
             Console.WriteLine("Bank Transfer Payment Processor processed the payment of {0:C}", amount);
         }
     }
-
+    //Represents the main application that includes how the chain will be created and how payment processing will be performed.
+    //Different payment processors are created for payment processing, and the chain is created by connecting these processors to each other.
+    //The processor in the first link of the chain is called the ProcessPayment method for payment processing, and cases where other processors
+    //in the chain try to process the transaction are checked.
     class Program
     {
         static void Main(string[] args)
